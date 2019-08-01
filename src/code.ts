0896b813db35bae178d0101d26bff544f10833aa
rishu007 <rishulakhotiya@gmail.com>
2019-08-02 00:42:17 +0530

    comment added

--- src/code.ts
+++ src/code.ts
@@ -2,5 +2,5 @@ import { of } from "rxjs";
 import { mapTo } from "rxjs/operators";
 const answer = of(6 * 9).pipe(mapTo(42));
 answer.subscribe(res=>{
-    console.log(res);
+    console.log(res);//comment
 })
